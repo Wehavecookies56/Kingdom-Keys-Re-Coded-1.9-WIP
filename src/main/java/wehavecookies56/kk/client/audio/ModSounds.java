e96a897bfbac07b1d554d4e3e717ceaa26c41308
Abelatox <abel54225@gmail.com>
2016-03-21 20:04:51 +0100

    sounds

--- src/main/java/wehavecookies56/kk/client/audio/ModSounds.java
+++ src/main/java/wehavecookies56/kk/client/audio/ModSounds.java
@@ -1,37 +1,39 @@
 package wehavecookies56.kk.client.audio;
 
+import net.minecraft.init.Bootstrap;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import wehavecookies56.kk.lib.Reference;
 
 public class ModSounds {
 
-	public static SoundEvent
-	kupo,
-	summon,
-	alarm,
-	unsummon,
-	sharpshooterbullet,
-	error,
-	select,
-	cancel,
-	move,
-	drive,
-	antidrive,
-	potion,
-	savepoint,
-	savespawn,
-	birthBySleepALinkToTheFuture
+	public static final SoundEvent kupo;
 	//TODO The rest of the sounds, records and music
 	;
 	
-	public static void init() {
-		kupo = getSound("kupo");
-	}
-	
-	public static SoundEvent getSound(String loc) {
-		SoundEvent soundEvent = SoundEvent.soundEventRegistry.getObject(new ResourceLocation(Reference.MODID, loc));
-		return soundEvent;
-	}
-	
+	private static SoundEvent getRegisteredSoundEvent(String id)
+    {
+        SoundEvent soundevent = (SoundEvent)SoundEvent.soundEventRegistry.getObject(new ResourceLocation(id));
+
+        if (soundevent == null)
+        {
+            throw new IllegalStateException("Invalid Sound requested: " + id);
+        }
+        else
+        {
+            return soundevent;
+        }
+    }
+
+    static
+    {
+        if (!Bootstrap.isRegistered())
+        {
+            throw new RuntimeException("Accessed Sounds before Bootstrap!");
+        }
+        else
+        {
+            kupo = getRegisteredSoundEvent("kupo");
+        }
+    }
 }
