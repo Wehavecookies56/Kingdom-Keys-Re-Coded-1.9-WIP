af33d66ec3b5fa382a790a731ccddcca07748ccc
Abelatox <abel54225@gmail.com>
2016-03-25 00:22:28 +0100

    I meant this :P

--- src/main/java/wehavecookies56/kk/network/packet/server/GiveItemInSlot.java
+++ src/main/java/wehavecookies56/kk/network/packet/server/GiveItemInSlot.java
@@ -10,63 +10,64 @@ import net.minecraftforge.fml.relauncher.Side;
 import wehavecookies56.kk.network.packet.AbstractMessage;
 
 public class GiveItemInSlot extends AbstractMessage<GiveItemInSlot> {
-	//String player;
 	ItemStack itemstack;
 	int slot;
 	double x,y,z;
-	boolean shouldDrop = false;
+	boolean alreadyHasItem;
 	public GiveItemInSlot () {}
 	
-	public GiveItemInSlot (ItemStack itemstack, int slot) 
+	public GiveItemInSlot (ItemStack itemstack, int slot, boolean alreadyHasItem) 
 	{
-		//this.player = player;
 		this.itemstack = itemstack;
 		this.slot = slot;
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;
+		this.alreadyHasItem = alreadyHasItem;
 	}
 	
-	public GiveItemInSlot (ItemStack itemstack, int slot, double x, double y, double z, boolean drop) 
+	public GiveItemInSlot (ItemStack itemstack, int slot, double x, double y, double z) 
 	{
-		//this.player = player;
 		this.itemstack = itemstack;
 		this.slot = slot;
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		this.shouldDrop = drop;
+		this.alreadyHasItem = false;
 	}
 	@Override
 	protected void read (PacketBuffer buffer) throws IOException {
-		//player = buffer.readStringFromBuffer(40);
 		itemstack = buffer.readItemStackFromBuffer();
 		slot = buffer.readInt();
 		x = buffer.readDouble();
 		y = buffer.readDouble();
 		z = buffer.readDouble();
-		shouldDrop = buffer.readBoolean();
+		alreadyHasItem = buffer.readBoolean();
 	}
 
 	@Override
 	protected void write (PacketBuffer buffer) throws IOException {
-		//buffer.writeString(player);
 		buffer.writeItemStackToBuffer(itemstack);
 		buffer.writeInt(slot);
 		buffer.writeDouble(x);
 		buffer.writeDouble(y);
 		buffer.writeDouble(z);
-		buffer.writeBoolean(shouldDrop);
+		buffer.writeBoolean(alreadyHasItem);
 	}
 
 	@Override
 	public void process (EntityPlayer player, Side side) 
 	{
-		//if(!shouldDrop){return;}
-		if(slot >= 0){
-			player.inventory.setInventorySlotContents(slot, itemstack);
-		} else {
-			player.worldObj.spawnEntityInWorld(new EntityItem(player.worldObj, x, y, z, itemstack));
+		if(alreadyHasItem)
+		{
+			player.inventory.addItemStackToInventory(itemstack);
+		}
+		else{
+			if(slot >= 0){
+				player.inventory.setInventorySlotContents(slot, itemstack);
+			} else {
+				player.worldObj.spawnEntityInWorld(new EntityItem(player.worldObj, x, y, z, itemstack));
+			}
 		}
 	
 	}
